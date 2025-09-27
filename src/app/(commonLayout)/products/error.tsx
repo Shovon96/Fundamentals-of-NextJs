9d9174da12b043f29435cfbed220975acb0c6cc3
Shovon96 <fakhruddinshovon@gmail.com>
2025-09-28 02:34:41 +0600

    Handle global errors with built-in error page

---
+++ src/app/(commonLayout)/products/error.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+export default function ProductErrorPage({ error, reset }: {
+    error: Error & { digest?: string }
+    reset: () => void
+}) {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-center px-4">
+            <h2 className="text-4xl font-semibold text-red-500 mb-2">Something went wrong!</h2>
+            <button
+                className="inline-block bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition"
+                onClick={
+                    () => reset()
+                }
+            >
+                Try again
+            </button>
+        </div>
+    )
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import Link from "next/link";
+
+export default function NotFoundPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-center px-4">
+            <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
+            <h2 className="text-4xl font-semibold mb-2">Page Not Found</h2>
+            <p className="text-gray-100 mb-6">
+                The page you are looking for could not be found.
+            </p>
+            <Link
+                href="/"
+                className="inline-block bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition"
+            >
+                Return Home
+            </Link>
+        </div>
+    );
+}
